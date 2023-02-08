89b85ed81b07f38f131b00dcc0da93133d904f1c
muhammedkadioglu <muhammedkadi9@gmail.com>
2023-02-08 13:43:18 +0300

    db.config

--- db.config.js
+++ db.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    HOST: "127.0.0.1",
-    PORT: "3306",
-    USER: "cms",
-    PASSWORD: "cms123",
-    DB: "Template",
-    dialect: "mysql",/* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  HOST: "DBHOST",
+  PORT: "DBPORT",
+  USER: "USERNAME",
+  PASSWORD: "PASSWORD",
+  DB: "EvaExchange",
+  dialect: "mysql",/* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
